b4e62a6eb15a32dea332f598b17f994d649916e4
Alan Grubb <alangrubb@outlook.com>
2023-08-25 13:56:00 -0700

    fixed dates

---
+++ Resources/Project2_select.sql
@@ -0,0 +1,97 @@
+drop table contacts
+CREATE TABLE "contacts" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR(30)  NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "email" VARCHAR(50)  NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+select * from contacts
+drop table campaign
+CREATE TABLE "campaign" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(100)  NOT NULL,
+    "goal" FLOAT  NOT NULL,
+    "pledged" FLOAT  NOT NULL,
+    "outcome" VARCHAR(30)  NOT NULL,
+    "backers_count" INTEGER  NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)  NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(30)   NOT NULL,
+    "subcategory_id" VARCHAR(30)  NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+select * from campaign
+drop table subcategory
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)  NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory_id" PRIMARY KEY (
+        "subcategory"
+     )
+);
+select * from subcategory
+drop table category
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10)  NOT NULL,
+    "category" VARCHAR(30)  NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+select * from category
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+drop table subcategory
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)  NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory_id" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+select * from subcategory
+drop table subcategory
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)  NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory_id" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+select * from subcategory
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+select * from subcategory
+
+DELETE FROM category;
+
+select * from category
+
+select * from contacts
+
+select * from campaign
+
+
+
+
+
+
--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -5,36 +5,36 @@
 -- Project 2
 
 CREATE TABLE "category" (
-    "category_id" VARHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(10)  NOT NULL,
+    "category" VARCHAR(30)  NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" INTEGER   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory"
+    "subcategory_id" VARCHAR(10)  NOT NULL,
+    "subcategory" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_subcategory_id" PRIMARY KEY (
+        "subcategory_id"
      )
 );
 
 CREATE TABLE "campaign" (
     "cf_id" INTEGER   NOT NULL,
     "contact_id" INTEGER   NOT NULL,
-    "company_name" VARCHAR   NOT NULL,
-    "description" VARCHAR   NOT NULL,
-    "goal" INTEGER   NOT NULL,
-    "pledged" INTEGER   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
-    "backers_count" VARCHAR   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
+    "company_name" VARCHAR(50)   NOT NULL,
+    "description" VARCHAR(100)  NOT NULL,
+    "goal" FLOAT  NOT NULL,
+    "pledged" FLOAT  NOT NULL,
+    "outcome" VARCHAR(30)  NOT NULL,
+    "backers_count" INTEGER  NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)  NOT NULL,
     "launched_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(30)   NOT NULL,
+    "subcategory_id" VARCHAR(30)  NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
@@ -42,9 +42,9 @@ CREATE TABLE "campaign" (
 
 CREATE TABLE "contacts" (
     "contact_id" INTEGER   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
+    "first_name" VARCHAR(30)  NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "email" VARCHAR(50)  NOT NULL,
     CONSTRAINT "pk_contacts" PRIMARY KEY (
         "contact_id"
      )
